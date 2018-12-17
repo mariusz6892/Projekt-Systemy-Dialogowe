cd87100271a5ca5a355e257e194c1071b23c0160
Mariusz Bińkowski <mariusz6892@gmail.com>
2018-12-17 23:44:45 +0100

    First Commit

---
+++ Obsługa Taxi/Obsługa Taxi/Models/Klient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obsługa_Taxi
+{
+    public class Klient
+    {
+        private int _Id_klient;
+        private String _Nr_telefonu;
+        private Boolean _IsLogged;
+
+        
+
+
+        Klient(int _Id_klient, string _Nr_telefonu)
+        {
+            Id_klient = _Id_klient;
+            Nr_telefonu = _Nr_telefonu;
+            IsLogged = true;
+        }
+
+        public int Id_klient { get => _Id_klient; set => _Id_klient = value; }
+        public String Nr_telefonu { get => _Nr_telefonu; set => _Nr_telefonu = value; }
+        public Boolean IsLogged { get => _IsLogged; set => _IsLogged = value; }
+
+
+        public void LogOut()
+        {
+            IsLogged = false;
+        }
+
+        public void LogIn()
+        {
+            IsLogged = true;
+        }
+    }
+}
---
+++ Obsługa Taxi/Obsługa Taxi/Models/Taxi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obsługa_Taxi
+{
+    class Taxi
+    {
+
+        private int _Id_taxi;
+        private int _Nr_taxi;
+        private String _Aktualny_adres;
+        private Boolean _IsBusy;
+
+
+
+
+        Taxi(int _Id_taxi, int _Nr_taxi, String _Aktualny_adres)
+        {
+            Id_taxi = _Id_taxi;
+            Aktualny_adres = _Aktualny_adres;
+            IsBusy = false;
+        }
+
+        public int Id_taxi { get => _Id_taxi; set => _Id_taxi = value; }
+        public String Aktualny_adres { get => _Aktualny_adres; set => _Aktualny_adres = value; }
+        public Boolean IsBusy { get => _IsBusy; set => _IsBusy = value; }
+        public int Nr_taxi { get => _Nr_taxi; set => _Nr_taxi = value; }
+
+        public void Drive(String _Adres_do)
+        {
+            IsBusy = true;
+            Aktualny_adres = _Adres_do;
+        }
+
+        public void EndTrip()
+        {
+            IsBusy = false;
+        }
+    }
+}
